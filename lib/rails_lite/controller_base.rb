9f97f67bd57cd8670456c0885d13754a129f0d46
Nicollette <nicolletteml@gmail.com>
2013-12-17 15:02:29 -0800

    Initial commit

---
+++ lib/rails_lite/controller_base.rb
@@ -0,0 +1,34 @@
+require 'erb'
+require_relative 'params'
+require_relative 'session'
+
+class ControllerBase
+  attr_reader :params
+
+  def initialize(req, res, route_params)
+    @req = req
+    @res = res
+    @route_params = route_params
+  end
+
+  def session
+  end
+
+  def already_rendered?
+  end
+
+  def redirect_to(url)
+  end
+
+  def render_content(content, type)
+    @res.content_type = type
+    @res.body = content
+    @already_built_response = true
+  end
+
+  def render(template_name)
+  end
+
+  def invoke_action(name)
+  end
+end

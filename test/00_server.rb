b9583ea71333e109bd950e90210aa31683fbadc5
Nicollette <nicolletteml@gmail.com>
2014-02-27 16:10:27 -0800

    removed comments

--- test/00_server.rb
+++ test/00_server.rb
@@ -11,12 +11,11 @@ trap('INT') { server.shutdown }
 
 class MyController < ControllerBase
   def go
-    # render_content("hello world!", "text/html")
-    # redirect_to('http://google.com')
-    # after you have template rendering, uncomment:
-    # render :show
+    render_content("hello world!", "text/html")
+    redirect_to('http://google.com')
+    after you have template rendering, uncomment:
+    render :show
 
-    # after you have sessions going, uncomment:
    session["count"] ||= 0
    session["count"] += 1
    render :counting_show
